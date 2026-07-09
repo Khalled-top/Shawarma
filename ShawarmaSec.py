267526051dda5b9101ad33067d3705bee588b68e
Khalled-top <hdhheg4@gmail.com>
2026-07-09 13:57:24 +0300

    go pro

---
+++ ShawarmaSec.py
@@ -0,0 +1,14 @@
+!#/bin/python3
+print("Starting Shawarma Security Scanner...")
+
+threats = [
+    "Recipe Theft",
+    "Fake Orders",
+    "Unauthorized Access"
+    "Shawarma without Tom sauce"
+]
+
+for threat in threats:
+    print("[WARNING!!]", threat)
+
+print("Security Scan Completed")
